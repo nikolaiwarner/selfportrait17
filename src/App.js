ee38bf7d33ee956b72a6e1f87003ee52ef4ea91a
Nick Warner <nickwarner@gmail.com>
2017-10-02 21:57:44 -0400

    Add a structure for automatic queuing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,82 @@ import 'aframe-chromakey-material'
 import {Entity, Scene} from 'aframe-react'
 import Fullscreen from 'react-html5-fullscreen'
 import React, { Component } from 'react'
+import uuid from 'uuid/v4'
 
 import './App.css'
 
 class App extends Component {
-  componentWillMount () {
+  constructor () {
+    super()
+    this.state = {
+      items: [],
+      items2: new Map(),
+      stories: [{
+        items: [
+          {src: 'smoke'},{src: 'smoke'},{src: 'smoke'},{src: 'smoke'},{src: 'smoke'},{src: 'smoke'},{src: 'smoke'},{src: 'smoke'},{src: 'smoke'},
+          {src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},
+        ]
+      }, {
+        items: [
+          {src: 'smoke'},
+          {src: 'character1'}
+        ]
+      }, {
+        items: [
+          {src: 'smoke'},
+          {src: 'character1'}
+        ]
+      }, {
+        items: [
+          {src: 'smoke'},
+          {src: 'character1'}
+        ]
+      }],
+      videos: [
+        {id: 'background1', src: 'b1.mp4'},
+        {id: 'character1', src: '1.mp4'},
+        {id: 'smoke', src: 'smoke1.mp4'}
+      ]
+    }
+  }
 
+  componentWillMount () {
+    this.playStory(0)
   }
 
   componentDidMount () {
     document.querySelectorAll('video').forEach((video) => { video.play() })
+  }
+
+  randomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  playStory (storyId) {
+    if (storyId === this.state.stories.length) {
+      storyId = 0
+    }
+    let timeout = 60 * 1000 + (this.randomInt(-30, 30) * 1000)
+    setTimeout(this.playStory.bind(this, storyId + 1), timeout)
+    this.state.stories[storyId].items.forEach((item) => {
+      setTimeout(() => {
+        const id = uuid()
+        this.setState((state) => {
+          item.id = id
+          const items = [...state.items, item]
+          return {items}
+        }, () => {
+          setTimeout(() => {
+            this.setState((state) => {
+              const stateItems = state.items.filter((stateItem) => {
+                return stateItem.id !== id
+              })
+              return {items: stateItems}
+            })
+          }, (this.randomInt(10, 60) * 1000))
+        })
+      }, (this.randomInt(0, 30) * 1000))
+    })
   }
 
   render () {
@@ -30,18 +96,20 @@ class App extends Component {
           fog='type: linear; color: #AAA; near'
         >
           <a-assets>
-            <video id='background1' className='video' src='b1.mp4' loop autoPlay={false} />
-            <video id='background2' className='video' src='b2.mp4' loop autoPlay={false} />
-            <video id='character1' className='video' src='1.mp4' loop autoPlay={false} />
-
-            <video id='smoke' className='video' src='smoke1.mp4' loop autoPlay={false} />
+            {this.state.videos.map((item) => (
+              <video key={item.id} autoPlay className={'video'} id={item.id} loop src={item.src} />
+            ))}
           </a-assets>
-          <Entity geometry='primitive: box' material='shader: chromakey; src: #background1; color: 1 0 0' scale='100 50 0' position='0 0 -20' rotation='0 0 0' />
-
-          <Entity geometry='primitive: box' material='shader: chromakey; src: #smoke; color: 0.0 1 0.0' scale='100 50 0' position='-10 1.5 -10' rotation='0 0 0' />
-
-          <Entity geometry='primitive: box' material='shader: chromakey; src: #character1; color: 0.0 0.9 0.1' scale='2.5 3 0' position='0 1.5 -2' rotation='0 0 0' />
-
+          {this.state.items.map((item) => (
+            <a-entity
+              key={item.id}
+              geometry={'primitive: box'}
+              material={`shader: chromakey; src: #${item.src}; color: ${item.color || '0 1 0'}`}
+              scale={`${this.randomInt(75, 100)} ${this.randomInt(75, 100)} 0`}
+              position={`${this.randomInt(-10, 10)} ${this.randomInt(-2, 2)} ${this.randomInt(-20, -1)}`}
+              rotation={`0 0 0`}
+            />
+          ))}
         </Scene>
       </div>
     )
