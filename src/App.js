3e87a86f38b9a70beabebfc31da3f87b00cf780f
Nick Warner <nickwarner@gmail.com>
2017-10-03 23:34:13 -0400

    done!

--- src/App.js
+++ src/App.js
@@ -1,43 +1,62 @@
 import 'aframe'
 import 'aframe-chromakey-material'
-import {Entity, Scene} from 'aframe-react'
+import {Scene} from 'aframe-react'
 import Fullscreen from 'react-html5-fullscreen'
 import React, { Component } from 'react'
 import uuid from 'uuid/v4'
+import {sortBy} from 'lodash'
 
 import './App.css'
+
+const ONE_SECOND = 1000
 
 class App extends Component {
   constructor () {
     super()
     this.state = {
       items: [],
-      items2: new Map(),
       stories: [{
         items: [
-          {src: 'smoke'},{src: 'smoke'},{src: 'smoke'},{src: 'smoke'},{src: 'smoke'},{src: 'smoke'},{src: 'smoke'},{src: 'smoke'},{src: 'smoke'},
-          {src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},{src: 'character1'},
+          {src: 'background1', minScaleX: 100, maxScaleX: 110, minPosZ: -25, maxPosZ: -28},
+          {src: 'background1', minScaleX: 100, maxScaleX: 110, minPosZ: -21, maxPosZ: -22},
+          {src: 'smoke'},
+          {src: 'smoke'},
+          {src: 'smoke'},
+          {src: 'smoke'},
+          {src: 'character1', minPosZ: -15, maxPosZ: -20},
+          {src: 'character1', minScaleX: 20, maxScaleX: 20, minScaleY: 25, maxScaleY: 25, minPosX: -2, maxPosX: 0, minPosY: 0, maxPosY: 3, minPosZ: -10, maxPosZ: -15}
         ]
       }, {
         items: [
-          {src: 'smoke'},
-          {src: 'character1'}
+          {src: 'self2', color: '0.5 0.5 0.5'},
+          {src: 'self2', color: '0.5 0 0.5'},
+          {src: 'self2', minPosZ: -20, maxPosZ: -25},
+          {src: 'self2', color: '0.5 0.5 0.5'},
+          {src: 'self2', color: '0.5 0 0.5'},
+          {src: 'self2', minPosZ: -20, maxPosZ: -25},
+          {src: 'self1', minScaleX: 20, maxScaleX: 30, minScaleY: 20, maxScaleY: 30, minPosX: 0, maxPosX: 0, minPosY: 0, maxPosY: 0, minPosZ: -10, maxPosZ: -15},
+          {src: 'self1', minPosZ: -15}
         ]
       }, {
         items: [
-          {src: 'smoke'},
-          {src: 'character1'}
-        ]
-      }, {
-        items: [
-          {src: 'smoke'},
-          {src: 'character1'}
+          {src: 'outside2', minScaleX: 100, maxScaleX: 110, minPosZ: -21, maxPosZ: -22},
+          {src: 'outside2', minScaleX: 100, maxScaleX: 110, minPosZ: -21, maxPosZ: -28},
+          {src: 'outside1', minScaleX: 50, maxScaleX: 50, minScaleY: 50, maxScaleY: 50, minPosX: 0, maxPosX: 0, minPosY: 0, maxPosY: 0, minPosZ: -10, maxPosZ: -15},
+          {src: 'outside1'},
+          {src: 'outside1'},
+          {src: 'outside3', minScaleX: 50, maxScaleX: 50, minScaleY: 75, maxScaleY: 75, minPosZ: -18, maxPosZ: -19},
+          {src: 'outside3', minScaleX: 50, maxScaleX: 50, minScaleY: 75, maxScaleY: 75, minPosZ: -18, maxPosZ: -19}
         ]
       }],
       videos: [
         {id: 'background1', src: 'b1.mp4'},
         {id: 'character1', src: '1.mp4'},
-        {id: 'smoke', src: 'smoke1.mp4'}
+        {id: 'smoke', src: 'smoke1.mp4'},
+        {id: 'self1', src: 'self1.mp4'},
+        {id: 'self2', src: 'self2.mp4'},
+        {id: 'outside1', src: 'outside1.mp4'},
+        {id: 'outside2', src: 'outside2.mp4'},
+        {id: 'outside3', src: 'outside3.mp4'}
       ]
     }
   }
@@ -58,15 +77,27 @@ class App extends Component {
     if (storyId === this.state.stories.length) {
       storyId = 0
     }
-    let timeout = 60 * 1000 + (this.randomInt(-30, 30) * 1000)
+    if (storyId === 0) {
+      this.setState({showTitle: true})
+      setTimeout(() => {
+        this.setState({showTitle: false})
+      }, 10 * ONE_SECOND)
+    }
+    let timeout = 60 * ONE_SECOND + (this.randomInt(-10, 10) * ONE_SECOND)
     setTimeout(this.playStory.bind(this, storyId + 1), timeout)
     this.state.stories[storyId].items.forEach((item) => {
       setTimeout(() => {
         const id = uuid()
         this.setState((state) => {
           item.id = id
-          const items = [...state.items, item]
-          return {items}
+          item.scale = `${this.randomInt(item.minScaleX || 50, item.maxScaleX || 100)} ${this.randomInt(item.minScaleY || 50, item.maxScaleY || 100)} 0`
+          let positionZ = this.randomInt(item.minPosZ || -20, item.maxPosZ || -1)
+          item.positionZ = positionZ
+          item.position = `${this.randomInt(item.minPosX || -10, item.maxPosX || 10)} ${this.randomInt(item.minPosY || -2, item.maxPosY || 2)} ` + positionZ
+          let items = [...state.items, item]
+          // The chromakeying requires elements to be stacked in Z order to see through an element
+          items = sortBy(items, 'positionZ').reverse()
+          return {items, storyId}
         }, () => {
           setTimeout(() => {
             this.setState((state) => {
@@ -75,42 +106,45 @@ class App extends Component {
               })
               return {items: stateItems}
             })
-          }, (this.randomInt(10, 60) * 1000))
+          }, (this.randomInt(10, 60) * ONE_SECOND))
         })
-      }, (this.randomInt(0, 30) * 1000))
+      }, (this.randomInt(0, 10) * ONE_SECOND))
     })
   }
 
   render () {
     return (
       <div>
-        <Fullscreen
-          contentEnter={<a href={'#'}>Enter fullscreen</a>}
-          contentExit={<div />}
-          target='body'
-        />
         <Scene
           embedded
           vr-mode-ui='enabled: false'
           className='scene'
-          fog='type: linear; color: #AAA; near'
         >
           <a-assets>
             {this.state.videos.map((item) => (
               <video key={item.id} autoPlay className={'video'} id={item.id} loop src={item.src} />
             ))}
           </a-assets>
+          <a-sky color="#00FF00"></a-sky>
           {this.state.items.map((item) => (
             <a-entity
               key={item.id}
               geometry={'primitive: box'}
               material={`shader: chromakey; src: #${item.src}; color: ${item.color || '0 1 0'}`}
-              scale={`${this.randomInt(75, 100)} ${this.randomInt(75, 100)} 0`}
-              position={`${this.randomInt(-10, 10)} ${this.randomInt(-2, 2)} ${this.randomInt(-20, -1)}`}
+              scale={item.scale}
+              position={item.position}
               rotation={`0 0 0`}
             />
           ))}
         </Scene>
+        <Fullscreen
+          contentEnter={<div>Enter fullscreen</div>}
+          contentExit={<div />}
+          target='body'
+        />
+        {this.state.showTitle &&
+          <div className='titles'>self portrait 17 <span className='by'> nick warner</span></div>
+        }
       </div>
     )
   }
